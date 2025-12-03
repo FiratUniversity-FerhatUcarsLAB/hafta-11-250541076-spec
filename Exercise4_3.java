5d9c2bb008b16820aa912596b9f240d2731ea2f9
nur <250541076@firat.edu.tr>
2025-12-03 21:49:07 +0300

    Exercise4_3.java

--- Exercise4_3.java
+++ Exercise4_3.java
@@ -1,24 +1,10 @@
 public class Exercise4_3 {
 
-    public static void zoop() {
-        baffle();
-        System.out.print("You wugga ");
-        baffle();
+    public static void ping(String note) {
+        System.out.println(note + " ping");
     }
 
     public static void main(String[] args) {
-        System.out.print("No, I ");
-        zoop();
-        System.out.print("I ");
-        baffle();
-    }
-
-    public static void baffle() {
-        System.out.print("wug");
-        ping();
-    }
-
-    public static void ping() {
-        System.out.println(".");
+        ping("welcome");
     }
 }
