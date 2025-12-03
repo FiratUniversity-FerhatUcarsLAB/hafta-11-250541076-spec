8156531dce758ba8d3dfe232b7cbca8e79444d41
nur <250541076@firat.edu.tr>
2025-12-03 21:50:38 +0300

    Exercise4_5.java

--- Exercise4_5.java
+++ Exercise4_5.java
@@ -2,26 +2,15 @@ public class Exercise4_5 {
 
     public static void zoop(String fred, int bob) {
         System.out.println(fred);
-        if (bob == 5) {
-            ping("not ");
-        } else {
-            System.out.println("!");
-        }
+        ping(bob * 2);
+    }
+
+    public static void ping(int a) {
+        System.out.println(a);
     }
 
     public static void main(String[] args) {
-        int bizz = 5;
-        int buzz = 2;
-        zoop("just for", bizz);
-        clink(2 * buzz);
-    }
-
-    public static void clink(int fork) {
-        System.out.print("It's ");
-        zoop("breakfast ", fork);
-    }
-
-    public static void ping(String strangStrung) {
-        System.out.println("any " + strangStrung + "more ");
+        zoop("Hello", 5);
+        zoop("World", 10);
     }
 }
