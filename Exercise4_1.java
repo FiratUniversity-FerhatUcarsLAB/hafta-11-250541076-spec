acf21b65a88c3d1342370699372386f8a0f52c2d
nur <250541076@firat.edu.tr>
2025-12-03 21:47:11 +0300

    Add date printing methods for American and European formats
    Implemented methods to print dates in American and European formats.

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,25 @@
 public class Exercise4_1 {
 
+    // Amerikan tarzı tarih yazdırma metodu
     public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+        System.out.println(day + ", " + month + " " + date + ", " + year);
     }
 
+    // Avrupa tarzı tarih yazdırma metodu
     public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+        System.out.println(day + " " + date + " " + month + " " + year);
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+
+        // Örnek değerler
+        String day = "Tuesday";
+        int date = 3;
+        String month = "December";
+        int year = 2024;
+
+        // Test çağrıları
+        printAmerican(day, date, month, year);
+        printEuropean(day, date, month, year);
     }
 }
